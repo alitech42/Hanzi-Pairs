db32afd07861ccdfae6ea0b1a3e79205fa771a27
ValienteRitter <tagizade2006f@gmail.com>
2025-11-09 22:02:34 +0400

    Feat: Create function to randomly select pinyin-hanzi pairs from data

--- src/utilities.jsx
+++ src/utilities.jsx
@@ -1,9 +1,17 @@
 import hsk from "./data/hsk.json";
 
 export function setHanziPairs(count, diffLevel) {
-    return hsk
-        .filter(({ level }) => level === diffLevel)
-        .slice(0, count)
+    const filteredHsk = hsk.filter(({ level }) => level === diffLevel);
+    const randomIndexArr = [];
+
+    for (let i = 0; i < count; i++) {
+        randomIndexArr.push(Math.floor(Math.random() * filteredHsk.length));
+    }
+
+    return filteredHsk
+        .filter((item, index) =>
+            randomIndexArr.some((randomIndex) => randomIndex === index)
+        )
         .map(({ pinyin, hanzi }) => ({ pinyin, hanzi }));
 }
 
